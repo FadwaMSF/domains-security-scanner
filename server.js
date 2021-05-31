756848b4e8bd862374d68da4209bb9511dcfd81c
MahmoudAbdo2020 <mahmoud.abdo@brussels.msf.org>
2021-05-31 16:52:38 +0200

    import our routes into server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // import `name` from "package name" is the same like const name = require('package name')
 import express from "express";
 import bodyParser from "body-parser";
+import routes from "./src/routes";
 import cors from "cors";
 
 // To be able to use variables in .env file
@@ -12,6 +13,9 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//giving the server access to routes
+app.use(routes);
 
 const port = process.env.PORT || 8000;
 
