7fe679e107b5546b9db71bbed8f3cd43335ae83e
MahmoudAbdo2020 <mahmoud.abdo@brussels.msf.org>
2021-05-31 17:43:21 +0200

    export module routes

--- src/routes/allDomains.js
+++ src/routes/allDomains.js
@@ -2,3 +2,5 @@ const router = require("express-promise-router")();
 const domainController = require("../controllers/domain.controller");
 
 router.get("/domains", domainController.allDomains);
+
+module.exports = router;
