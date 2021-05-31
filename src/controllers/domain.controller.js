8a414bde39acc4ab412550b039916f95ec13fc88
MahmoudAbdo2020 <mahmoud.abdo@brussels.msf.org>
2021-05-31 17:57:48 +0200

    remove console.log

--- src/controllers/domain.controller.js
+++ src/controllers/domain.controller.js
@@ -3,11 +3,7 @@ import db from "../config/database";
 
 exports.allDomains = async (req, res) => {
   const { rows } = await db.query("SELECT * FROM t_domain");
-  console.log(rows);
   res.status(200).send({
     body: rows,
-    // {
-    //   domain: { domain, owner },
-    // },
   });
 };
