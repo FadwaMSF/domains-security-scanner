c858d81fb702ebfb343f96ea4649798f3b124cc6
MahmoudAbdo2020 <mahmoud.abdo@brussels.msf.org>
2021-05-31 16:18:03 +0200

    importing express and cors into server file

---
+++ server.js
@@ -0,0 +1,4 @@
+//Importing packages into our file
+// import `name` from "package name" is the same like const name = require('package name')
+import express from "express";
+import cors from "cors";
