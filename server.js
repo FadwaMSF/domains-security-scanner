0f12ab54773a2682e51b1dac76e46ad9ee0fc839
MahmoudAbdo2020 <mahmoud.abdo@brussels.msf.org>
2021-05-31 16:21:37 +0200

    adding .env config

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 //Importing packages into our file
 // import `name` from "package name" is the same like const name = require('package name')
 import express from "express";
+import bodyParser from "body-parser";
 import cors from "cors";
+
+// To be able to use variables in .env file
+require("dotenv").config();
 
 //Initializing the server
 const app = express();
